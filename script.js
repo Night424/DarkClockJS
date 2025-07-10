1dd8ba509bae509802babe80aba59469872035ff
Night <keanevgps3@gmail.com>
2025-07-10 17:17:02 +0200

    js script to update clock

---
+++ script.js
@@ -0,0 +1,20 @@
+// Func to update clock
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById ("clock").textContent = timeString;
+}
+
+// Update clock every second
+setInterval(updateClock, 1000);
+updateClock(); //call
+
+// Theme toggle
+const toggleBtn = document.getElementById("toggleTheme");
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
